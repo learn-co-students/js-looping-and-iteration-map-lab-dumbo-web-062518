c3d948fb6dd12f139440a5beaf91d0eb23e81188
Dan Chung <danetix@gmail.com>
2018-08-08 20:31:25 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution in this file.
+function lowerCaseDrivers(collection) {
+  return collection.map(item => item.toLowerCase())
+}
+
+function nameToAttributes(collection) {
+  return collection.map(item => {
+    const name = item.split(" ")
+    return Object.assign({}, {firstName: name[0], lastName: name[1]})
+  })
+}
+
+function attributesToPhrase(collection) {
+  return collection.map(item => `${item.name} is from ${item.hometown}`)
+}
